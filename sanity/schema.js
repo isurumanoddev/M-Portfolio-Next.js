febdf9e1b6c57ed4b615f12f3ae043565b2b6712
isuru <isurumanod99@gmail.com>
2023-06-28 09:04:27 +0530

    sanity configuration further

--- sanity/schema.js
+++
@@ -1,8 +0,0 @@
-
-import pageInfo from './schemas/pageInfo'
-import projects from './schemas/projects'
-import skill from './schemas/skill'
-
-export const schema = {
-  types: [pageInfo, projects, skill],
-}
