9c52c802569d70d0d83716472703786c0f03f91d
isurumnaod <isurumanod99@gmail.com>
2023-07-06 15:38:52 +0530

    Redesign project card for better user experience

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -11,7 +11,7 @@ function ProjectCard({name, description, live, github, image}) {
 
     return (
         <article
-            className={"bg-[#131b30] backdrop-blur-md snap-center w-full md:w-[500px]  xl:w-[600px] h-full  flex flex-col items-center space-y-4 flex-shrink-0"}>
+            className={"bg-[#131b30] backdrop-blur-md snap-center w-full md:w-[500px]  xl:w-[600px]   flex flex-col items-center space-y-4 flex-shrink-0"}>
 
 
                 <motion.img
@@ -19,7 +19,7 @@ function ProjectCard({name, description, live, github, image}) {
                     whileInView={{opacity: 1, y: 0}}
                     transition={{duration: 1.2}}
                     viewport={{once: false}}
-                    className={"rounded-md overflow-hidden object-cover w-[300px] md:w-[350px] lg:w-[450px] p-1 "}
+                    className={"min-h-4/10 rounded-md  object-cover w-[300px] md:w-[350px] lg:w-[450px] p-1 "}
                     src={image ?  urlFor(image).url() : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzSc0E_-ezcw1juku7x_q9rIVtGDEFGDsZnA&usqp=CAU" }
                 />
 
@@ -30,15 +30,15 @@ function ProjectCard({name, description, live, github, image}) {
                 viewport={{once: false}}
 
 
-                className={"px-1 md:px-10 flex flex-col gap-3 pb-3 text-center"}>
+                className={"px-1 md:px-10 flex flex-col gap-3 pb-3  text-center"}>
                 <h4 className={"text-lg lg:text-[20px] tracking-[10px]"}>{name}</h4>
 
-                <div className={" text-center text-[14px]"}>
+                <div className={" text-center text-[14px]   "}>
                     {description}
 
                 </div>
 
-                <div className={"flex flex-row flex-wrap gap-2 px-2"}>
+                <div className={" flex-row flex-wrap gap-2 px-2 hidden md:flex"}>
 
 
                     <p className={"px-2 py-1 font-semibold text-[15px] bg-[#E76161]/80 text-white rounded-full w-fit"}>Next
